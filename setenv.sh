a475c69157c1da8cf1a2955f49cd0e48e3b3c0da
Tom Crimi <tom@quantbot.com>
2025-08-03 17:19:23 -0400

    initial commit of s3redirect://

---
+++ setenv.sh
@@ -0,0 +1,5 @@
+export GCC=/tools/gcc/redhat/7/11.2.0
+export PATH=/tools/binutils/redhat/7/2.36.1/bin:/tools/conan/redhat/7/1.62.0/bin/:/tools/cmake/redhat/7/3.23.0/bin:/tools/ninja/redhat/7/1.11.0/bin:/tools/git/redhat/7/2.39.1/bin:$GCC/bin:/usr/bin
+export LD_LIBRARY_PATH=$GCC/lib64
+export CC=$GCC/bin/gcc
+export CXX=$GCC/bin/g++
